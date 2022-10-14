3b99bb0d322e1cb7877ea44c9775e5707cf0cd01
XiaoRik <xxd0225@gmail.com>
2022-10-14 08:59:05 +0800

    upate PEP style

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-# coding=utf-8
-import json, requests
-from sqlite3 import Timestamp
-from datetime import datetime, timedelta 
+import json
+from datetime import datetime, timedelta
+import requests
 from ics import Calendar, DisplayAlarm, Event
 
-class Date_Kit:
+
+class DateKit:
     def __init__(self) -> None:
         self.now = datetime.now()
         self.next = self.now + timedelta(days=30)
         self.start = self.now.strftime("%Y-%m-%d")
         self.end = self.next.strftime("%Y-%m-%d")
-        
-        self.start_ts = int(datetime.strptime(self.start, '%Y-%m-%d').timestamp())
+
+        self.start_ts = int(datetime.strptime(
+            self.start, '%Y-%m-%d').timestamp())
         self.end_ts = int(datetime.strptime(self.end, '%Y-%m-%d').timestamp())
         self.now_ts = int(datetime.timestamp(self.now))*1000
 
-dt = Date_Kit()
 
+dt = DateKit()
 
-# def get_time():
-#     dt_obj = datetime.now()
-#     end_obj = dt_obj + timedelta(days=1)
-#     start = dt_obj.strftime("%Y-%m-%d")
-#     end = end_obj.strftime("%Y-%m-%d")
-#     return start, end
 
 def get_response():
+    """
+    return kezhuanzhai response
+    """
     headers = {
         'authority': 'www.jisilu.cn',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -45,33 +43,38 @@ def get_response():
     }
 
     params = {
-    'qtype': 'CNV',
-    'start': f'{dt.start_ts}',
-    'end': f'{dt.end_ts}',
-    '_': f'{dt.now_ts}',
+        'qtype': 'CNV',
+        'start': f'{dt.start_ts}',
+        'end': f'{dt.end_ts}',
+        '_': f'{dt.now_ts}',
     }
-    response = requests.get('https://www.jisilu.cn/data/calendar/get_calendar_data/', params=params, headers=headers)
+    response = requests.get(
+        'https://www.jisilu.cn/data/calendar/get_calendar_data/', params=params, headers=headers)
     return json.loads(response.text)
 
+
 def main():
-    c = Calendar()
+    """
+    Generate ics file
+    """
+    c_ins = Calendar()
     for i in get_response():
-        if  '申购日' in i.get('title'):
+        if '申购日' in i.get('title'):
             print(i)
             start_date = datetime.strptime(dt.start, '%Y-%m-%d').date()
             if start_date >= datetime.now().date():
-                e = Event()
-                e.name  = i.get('title')
+                evt = Event()
+                evt.name = i.get('title')
                 # 9:30 - 11:30
-                # e.begin = data['STARTDATE'].replace("T00:00:00", " 01:30:00")  #DTEND:20210415T193000Z
-                # e.end   = data['STARTDATE'].replace("T00:00:00", " 03:30:00") 
-                e.begin = i.get('start') + ' 01:30:00'
-                e.end = i.get('start') + ' 03:30:00'
-                e.alarms.append(DisplayAlarm(trigger=timedelta(minutes=30), display_text=e.name)) # 10:00
-                c.events.add(e)
-           
-    with open('kzz.ics', 'w') as my_file:
-        my_file.writelines(c)
-        
+                evt.begin = i.get('start') + ' 01:30:00'
+                evt.end = i.get('start') + ' 05:30:00'
+                evt.alarms.append(DisplayAlarm(trigger=timedelta(
+                    minutes=30), display_text=evt.name))  # 10:00
+                c_ins.events.add(evt)
+
+    with open('kzz.ics', 'w',encoding='utf-8') as my_file:
+        my_file.writelines(c_ins.serialize())
+
+
 if __name__ == '__main__':
     main()
